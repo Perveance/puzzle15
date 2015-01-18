0879752e8c08b3dc88ee3db1390f1f40a6e55c0a
Perveance <mszhidko@gmail.com>
2015-01-18 12:49:07 +0100

    Adding sliding navigation drawer

--- app/src/main/java/mszhidko/games/puzzle15/BoardActivity.java
+++ app/src/main/java/mszhidko/games/puzzle15/BoardActivity.java
@@ -1,6 +1,9 @@
 package mszhidko.games.puzzle15;
 
 import mszhidko.games.movingtile.R;
+import mszhidko.games.puzzle15.adapter.NavDrawerListAdapter;
+import mszhidko.games.puzzle15.model.NavDrawerItem;
+
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -11,7 +14,9 @@ import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.util.TypedValue;
@@ -24,8 +29,11 @@ import android.view.ViewGroup;
 import android.view.ViewGroup.MarginLayoutParams;
 import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
 public class BoardActivity extends ActionBarActivity {
@@ -40,11 +48,93 @@ public class BoardActivity extends ActionBarActivity {
 	private static Board mStartBoard; // TODO: get rid of static keyword
     private static Solution mSolution; // TODO: Why this should be static?
 	private PuzzleFragment mBoardFrag;
-	
-	@Override
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    // nav drawer title
+    private CharSequence mDrawerTitle;
+
+    // used to store app title
+    private CharSequence mTitle;
+
+    // slide menu items
+    private String[] navMenuTitles;
+    //private TypedArray navMenuIcons;
+
+    private ArrayList<NavDrawerItem> navDrawerItems;
+    private NavDrawerListAdapter adapter;
+
+    @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_board);
+
+        mTitle = mDrawerTitle = getTitle();
+
+        // load slide menu items
+        navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items);
+
+        // nav drawer icons from resources
+        //navMenuIcons = getResources()
+        //        .obtainTypedArray(R.array.nav_drawer_icons);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.list_slidermenu);
+
+        navDrawerItems = new ArrayList<NavDrawerItem>();
+
+        // adding nav drawer items to array
+        // Home
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[0], 0));
+        // Find People
+        navDrawerItems.add(new NavDrawerItem(navMenuTitles[1], 0));
+        // Photos
+        //navDrawerItems.add(new NavDrawerItem(navMenuTitles[2], 0));
+        // Communities, Will add a counter here
+        //navDrawerItems.add(new NavDrawerItem(navMenuTitles[3], 0));
+        // Pages
+        //navDrawerItems.add(new NavDrawerItem(navMenuTitles[4], 0));
+        // What's hot, We  will add a counter here
+        //navDrawerItems.add(new NavDrawerItem(navMenuTitles[5], 0));
+
+        // Recycle the typed array
+        //navMenuIcons.recycle();
+
+        // setting the nav drawer list adapter
+        adapter = new NavDrawerListAdapter(getApplicationContext(),
+                navDrawerItems);
+        mDrawerList.setAdapter(adapter);
+
+        // enabling action bar app icon and behaving it as toggle button
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.drawable.ic_launcher,  //TODO: should be ic_drawer ! nav menu toggle icon
+                R.string.app_name, // nav drawer open - description for accessibility
+                R.string.app_name // nav drawer close - description for accessibility
+        ) {
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(mTitle);
+                // calling onPrepareOptionsMenu() to show action bar icons
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(mDrawerTitle);
+                // calling onPrepareOptionsMenu() to hide action bar icons
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        if (savedInstanceState == null) {
+            // on first time display view for first nav item
+            //displayView(0);
+        }
+
 
         mDim = getIntent().getIntExtra(GameMenuActivity.PUZZLE_DIMENTION, 3);
 		mStartBoard = (Board) getIntent().getSerializableExtra(GameMenuActivity.PUZZLE);
